d1e586431246dadb2c01b41db4a4e9875a4a0bfd
qutubshaikh <167284374+qutubshaikh@users.noreply.github.com>
2025-03-06 03:03:47 +0530

    first commit

---
+++ boolien cha[ter/boolean.js
@@ -0,0 +1,5 @@
+const gaise = (a, b) =>{
+    return a+b;
+}
+
+ 
---
+++ project/script.js
@@ -0,0 +1,32 @@
+var a = document.getElementById("loginbtn");
+var b = document.getElementById("registerbtn");
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+
+function login(){
+    x.style.left = "4px";
+    y.style.right = "-520px";
+    a.className += "white-btn";
+    b,className = "btn";
+    x.style.opacity =1;
+    y.style.opacity =0;
+}
+
+function register(){
+    x.style.left = "-510px"
+    y.style.right = "5px";
+    a.className += "btn";
+    b,className = "white-btn";
+    x.style.opacity =0;
+    y.style.opacity =1;
+}
+function mymenufunction(){
+    var i=document.getElementById("navmanu");
+   
+    if(i.className === "nav-manu"){
+        i.className += "responsive";
+
+    }else{
+        i.className = " nav-manu";
+    }
+}
